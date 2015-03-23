c96365218daf22a2924947d59d86c976eee79f13
igutierrez <igutierrez@emergya.com>
2015-03-23 09:46:32 +0100

    Changes in GenericHibernateDAOImpl.java. Added a method for applying parameters to sql queries

--- persistence-geo-core/src/main/java/com/emergya/persistenceGeo/dao/impl/GenericHibernateDAOImpl.java
+++ persistence-geo-core/src/main/java/com/emergya/persistenceGeo/dao/impl/GenericHibernateDAOImpl.java
@@ -30,9 +30,11 @@ package com.emergya.persistenceGeo.dao.impl;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Criteria;
 import org.hibernate.LockMode;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.DetachedCriteria;
@@ -40,124 +42,136 @@ import org.hibernate.criterion.Example;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import com.emergya.persistenceGeo.dao.GenericDAO;
 
 /**
  * Based on http://community.jboss.org/docs/DOC-13955
- * @param <T> entity type
- * @param <ID> primary key
+ * 
+ * @param <T>
+ *            entity type
+ * @param <ID>
+ *            primary key
  */
-public abstract class GenericHibernateDAOImpl<T, ID extends Serializable> extends HibernateDaoSupport implements GenericDAO<T, ID> {
+public abstract class GenericHibernateDAOImpl<T, ID extends Serializable>
+		extends HibernateDaoSupport implements GenericDAO<T, ID> {
 
 	protected Class<T> persistentClass;
 
 	@Autowired
-    public void init(SessionFactory sessionFactory) {
-        setSessionFactory(sessionFactory);
-    }
+	@Qualifier("sessionFactory")
+	public void init(SessionFactory sessionFactory) {
+		setSessionFactory(sessionFactory);
+	}
 
-    @SuppressWarnings({"unchecked"})
-    public GenericHibernateDAOImpl() {
-        try {
-            persistentClass = (Class<T>)
-                    ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-        } catch (ClassCastException e) {
-            //can be raised when DAO is inherited twice
-            persistentClass = (Class<T>)
-                    ((ParameterizedType) getClass().getSuperclass().getGenericSuperclass()).getActualTypeArguments()[0];
-        }
-    }
+	@SuppressWarnings({ "unchecked" })
+	public GenericHibernateDAOImpl() {
+		try {
+			persistentClass = (Class<T>) ((ParameterizedType) getClass()
+					.getGenericSuperclass()).getActualTypeArguments()[0];
+		} catch (ClassCastException e) {
+			// can be raised when DAO is inherited twice
+			persistentClass = (Class<T>) ((ParameterizedType) getClass()
+					.getSuperclass().getGenericSuperclass())
+					.getActualTypeArguments()[0];
+		}
+	}
 
 	public T findById(Long id, boolean lock) {
 		T entity;
-        if (lock) {
-            entity = (T) getHibernateTemplate().get(persistentClass, id, LockMode.PESSIMISTIC_WRITE);
-        } else {
-            entity = (T) getHibernateTemplate().get(persistentClass, id);
-        }
+		if (lock) {
+			entity = (T) getHibernateTemplate().get(persistentClass, id,
+					LockMode.PESSIMISTIC_WRITE);
+		} else {
+			entity = (T) getHibernateTemplate().get(persistentClass, id);
+		}
 
 		return entity;
 	}
 
-        @Override
+	@Override
 	public List<T> findAll() {
 		return findByCriteria();
 	}
 
-    @SuppressWarnings("unchecked")
-        @Override
-	public List<T> findAllFromTo(Integer first, Integer last){
-    	Criteria criteria = getSession().createCriteria(persistentClass);
-    	criteria.setFirstResult(first);
-    	criteria.setMaxResults(last-first);
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findAllFromTo(Integer first, Integer last) {
+		Criteria criteria = getSession().createCriteria(persistentClass);
+		criteria.setFirstResult(first);
+		criteria.setMaxResults(last - first);
 		return criteria.list();
-    }
-    
-    @SuppressWarnings("unchecked")
-        @Override
-    public List<T> findOrdered(Integer first, Integer last, String orderField, boolean ascending) {
-    	Criteria criteria = getSession().createCriteria(persistentClass);
-    	criteria.setFirstResult(first);
-    	criteria.setMaxResults(last-first);
-    	
-    	if(ascending){
-    		criteria.addOrder(Order.asc(orderField));
-    	} else {
-    		criteria.addOrder(Order.desc(orderField));
-    	}
-		return criteria.list();
-    }
+	}
 
 	@SuppressWarnings("unchecked")
-        @Override
-	public List<T> findByExample(T exampleInstance, String[] excludedProperties) {
-       return this.findByExample(exampleInstance, excludedProperties, false);
-	}
-	
-        @Override
-	public List<T> findByExample(T exampleInstance, String[] excludedProperties, boolean ignoreCase) {
-		 DetachedCriteria crit = DetachedCriteria.forClass(persistentClass);
-	        Example example = Example.create(exampleInstance);
-                
-                if(excludedProperties!=null) {
-			for (String exclude : excludedProperties) {
-				example.excludeProperty(exclude);
-			}                 
-                }
-			
-			if(ignoreCase) {
-				crit.add(example.ignoreCase());
-			} else {
-				crit.add(example);
-			}
-			
-			return getHibernateTemplate().findByCriteria(crit);
+	@Override
+	public List<T> findOrdered(Integer first, Integer last, String orderField,
+			boolean ascending) {
+		Criteria criteria = getSession().createCriteria(persistentClass);
+		criteria.setFirstResult(first);
+		criteria.setMaxResults(last - first);
+
+		if (ascending) {
+			criteria.addOrder(Order.asc(orderField));
+		} else {
+			criteria.addOrder(Order.desc(orderField));
+		}
+		return criteria.list();
 	}
 
-        @Override
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByExample(T exampleInstance, String[] excludedProperties) {
+		return this.findByExample(exampleInstance, excludedProperties, false);
+	}
+
+	@Override
+	public List<T> findByExample(T exampleInstance,
+			String[] excludedProperties, boolean ignoreCase) {
+		DetachedCriteria crit = DetachedCriteria.forClass(persistentClass);
+		Example example = Example.create(exampleInstance);
+
+		if (excludedProperties != null) {
+			for (String exclude : excludedProperties) {
+				example.excludeProperty(exclude);
+			}
+		}
+
+		if (ignoreCase) {
+			crit.add(example.ignoreCase());
+		} else {
+			crit.add(example);
+		}
+
+		return getHibernateTemplate().findByCriteria(crit);
+	}
+
+	@Override
 	public T makePersistent(T entity) {
-            // Fix for #87143 and other "hibernate session contains the object" problems
-            // by checking that, and merging instead of updating in that case.
-            if(getHibernateTemplate().contains(entity)){
-               return  getHibernateTemplate().merge(entity);
-            }else {
-                getHibernateTemplate().saveOrUpdate(entity);
-            }
-                
+		// Fix for #87143 and other "hibernate session contains the object"
+		// problems
+		// by checking that, and merging instead of updating in that case.
+		if (getHibernateTemplate().contains(entity)) {
+			return getHibernateTemplate().merge(entity);
+		} else {
+			getHibernateTemplate().saveOrUpdate(entity);
+		}
+
 		return entity;
 	}
 
-        @Override
+	@Override
 	public void makeTransient(T entity) {
 		getHibernateTemplate().delete(entity);
 	}
 
-        @Override
-    public Long getResults(){
-    	return (Long) getSession().createCriteria(persistentClass).setProjection(Projections.count("id")).uniqueResult();
-    }
+	@Override
+	public Long getResults() {
+		return (Long) getSession().createCriteria(persistentClass)
+				.setProjection(Projections.count("id")).uniqueResult();
+	}
 
 	/**
 	 * Use this inside subclasses as a convenience method.
@@ -169,6 +183,27 @@ public abstract class GenericHibernateDAOImpl<T, ID extends Serializable> extend
 			crit.add(c);
 		}
 		return getHibernateTemplate().findByCriteria(crit);
+	}
+
+	/**
+	 * Método que se encarga de aplicar un mapa de parametros a una Query dada.
+	 * 
+	 * @param query
+	 *            Query donde aplicar los parametros.
+	 * @param parametros
+	 *            Parametros que se deben aplicar.
+	 */
+	protected final void aplicarParametros(final Query query,
+			final Map<String, Object> parametros) {
+		if (query == null) {
+			throw new IllegalArgumentException("query");
+		}
+		if (parametros == null) {
+			throw new IllegalArgumentException("parametros");
+		}
+		for (final Map.Entry<String, Object> entry : parametros.entrySet()) {
+			query.setParameter(entry.getKey(), entry.getValue());
+		}
 	}
 
 }
